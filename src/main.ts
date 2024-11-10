efd75c3623f1114c3243c37dc31303557455e38f
indiciez <justinmajura@gmail.com>
2024-11-10 21:22:49 +0300

    Remove app modules and integrate microservices
    Removed AppController and AppService from both auth and user services. Introduced microservice connection and validation pipes in main.ts files, and registered microservice clients in respective modules. Updated package dependencies accordingly.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {Transport} from "@nestjs/microservices";
+import {ValidationPipe} from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      port: 3001,
+    }
+  })
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap().then(() => console.log('Auth server started'));
